94eb7416829117961d14533c4aebf655f80bb999
Manav Delvadiya <24010101056@darshan.ac.in>
2026-02-20 19:12:09 +0530

    Changes Some files

--- Todo-App/app/(auth)/register/page.js
+++ Todo-App/app/(auth)/register/page.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { type } from "./../../../../prismademo/src/generated/prisma/commonInputTypes";
 
 export default function RegisterPage() {
   const router = useRouter();
@@ -78,6 +79,39 @@ export default function RegisterPage() {
             className="w-full bg-gradient-to-r from-blue-500 to-purple-600 text-white py-2 rounded-md font-medium hover:opacity-90"
           >
             Register
+          </button>
+
+          {/* Google-Signup */}
+          <button
+            type="submit"
+            className="flex items-center justify-center gap-2 w-full border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-sm font-medium text-gray-700 dark:text-white py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 transition"
+          >
+            {/* Google logo */}
+            <svg
+              width="20"
+              height="20"
+              viewBox="-3 0 262 262"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid"
+            >
+              <path
+                d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+                fill="#4285F4"
+              />
+              <path
+                d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+                fill="#34A853"
+              />
+              <path
+                d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
+                fill="#FBBC05"
+              />
+              <path
+                d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+                fill="#EB4335"
+              />
+            </svg>
+            Sign up with Google
           </button>
         </form>
         <p className="mt-4 text-center text-sm text-gray-600 dark:text-gray-400">
--- Todo-App/app/api/register/route.js
+++ Todo-App/app/api/register/route.js
@@ -1,12 +1,21 @@
 import { connectDB } from "@/lib/connectDB";
 import User from "@/models/user.model.js";
 import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
   await connectDB();
   const user = await request.json();
   try {
     const { name, email, password } = user;
+    
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "All Fields Are Required.. " },
+        { status: 401 },
+      );
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     await User.create({
       name,
