3f038ec883989be9b9d681a9419911aba646360d
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-02-22 19:38:26 +0530

    Add higher Order func asyncHandler to handle all Error

--- auth-flow/src/utils/asyncHandler.ts
+++ auth-flow/src/utils/asyncHandler.ts
@@ -1,28 +1,54 @@
 import { NextResponse } from "next/server";
 import { ApiError } from "./ApiError";
 
-export const asyncHandler = (fn: Function) => {
-  async (req: Request, context?: any) => {
-    try {
-      return await fn(req, context);
-    } catch (error: any) {
-      console.log("Error ===>", error);
+export async function asyncHandler(req: Request, handler: Function) {
+  //! try
+  try {
+    return await handler(req);
+  } catch (error) {
+    //! custom error
+    console.log("Error InstanceOf  ApiError ?? ==", error instanceof ApiError);
 
-      if (error instanceof ApiError) {
-        return NextResponse.json(
-          { success: false, message: error.message },
-          { status: error.statusCode },
-        );
-      }
+    console.log("Error ====>", error);
+    console.log("Error Message ====>", error.message);
 
+    if (error instanceof ApiError) {
       return NextResponse.json(
-        {
-          success: false,
-          message: "Internal Server Error",
-          error: error.message,
-        },
-        { status: 500 },
+        { success: false, message: error.message },
+        { status: error.statusCode },
       );
     }
-  };
-};
+
+    //! unknown error
+    return NextResponse.json(
+      { success: false, message: "Something went wrong" },
+      { status: 500 },
+    );
+  }
+}
+
+// import { NextResponse } from "next/server";
+
+// export async function asyncHandler(req: Request, handler: Function) {
+//   try {
+//     return await handler(req);
+//   } catch (error: any) {
+
+//     console.log("Is ApiError =", error?.isApiError);
+
+//     if (error?.isApiError) {
+//       return NextResponse.json(
+//         {
+//           success: false,
+//           message: error.message,
+//         },
+//         { status: error.statusCode }
+//       );
+//     }
+
+//     return NextResponse.json(
+//       { success: false, message: "Something went wrong" },
+//       { status: 500 }
+//     );
+//   }
+// }
