5672e8db22c15fb86c08e12b1b17e7a6ad502919
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-04-04 21:56:03 +0530

    Add API routes for student data retrieval and creation

---
+++ lab-23/src/app/api/student/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { students } from "../data";
+
+export async function GET() {
+  return NextResponse.json(students);
+}
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+
+  const maxId = students.length ? Math.max(...students.map((s) => s.id)) : 0;
+
+  const newStudent = {
+    id: maxId + 1,
+    name: data.name,
+  };
+
+  students.push(newStudent);
+  return NextResponse.json(
+    {
+      newStudent,
+    },
+    { status: 201 },
+  );
+}
