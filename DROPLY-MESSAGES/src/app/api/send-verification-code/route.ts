55a8b41dd090307e3bdb3241b59745f9f033f10a
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-05-10 20:38:21 +0530

    Add POST API route to send verification code with user validation and email sending

---
+++ DROPLY-MESSAGES/src/app/api/send-verification-code/route.ts
@@ -0,0 +1,45 @@
+import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
+import { connectDB } from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  await connectDB();
+
+  const { identifier } = await req.json();
+
+  const user = await UserModel.findOne({
+    $or: [{ email: identifier }, { username: identifier }],
+  });
+
+  if (!user) {
+    return NextResponse.json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  if (user.isVerified) {
+    return NextResponse.json({
+      success: false,
+      message: "User is already verified Please login.",
+    });
+  }
+
+  const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+  const verifyCodeExpiry = new Date(Date.now() + 10 * 60 * 1000);
+
+  user.verifyCode = verifyCode;
+  user.verifyCodeExpiry = verifyCodeExpiry;
+
+  await user.save();
+
+  await sendVerificationEmail(user.username, user.email, verifyCode);
+
+  return NextResponse.json({
+    success: true,
+    message: "Verification code sent",
+  });
+
+}
